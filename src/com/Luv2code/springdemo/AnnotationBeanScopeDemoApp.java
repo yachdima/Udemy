2c9d16195f100654d84b7f60bcabdcd62d3e1730
sony <sony@VAIO>
2020-01-19 17:07:26 +0200

    les83

---
+++ src/com/Luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,32 @@
+package com.Luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load  sprinf conf file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		// close the context
+		context.close();
+
+	}
+
+}
--- src/com/Luv2code/springdemo/TennisCoach.java
+++ src/com/Luv2code/springdemo/TennisCoach.java
@@ -1,19 +1,36 @@
 package com.Luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
-	@Qualifier("myFortuneService")
+	@Qualifier("fileFortuneService")
 	private FortuneService fortuneService;
 	
 	//define a default constructor
 	public TennisCoach() {
 		System.out.println(">> inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennesCoach: inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennesCoach: inside of doMyCleanupStuff()");
 	}
 	
 	//define a setter method
